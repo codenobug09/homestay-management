9bfb379fceca05ad86df4b547b3c9c5a0f7757c4
chaunm <bangcac24@gmail.com>
2026-04-01 07:56:50 +0700

    homestay-init

---
+++ includes/dbconn.php
@@ -0,0 +1,7 @@
+<?php
+    $dbuser="root";
+    $dbpass="";
+    $host="127.0.0.1:3307";
+    $db="hostelmsphp";
+    $mysqli =new mysqli($host,$dbuser, $dbpass, $db);
+?>
---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<footer class="footer text-center text-muted">
+&copy; <?php echo date("Y"); ?> - Homestay Management System - Developed by <a href="https://LMC.com">LMC</a>
+</footer>
